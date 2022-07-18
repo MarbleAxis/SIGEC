72e96e100a86be58f020c608409ef4e6361762fa
MarbleAxis <76003127+MarbleAxis@users.noreply.github.com>
2022-07-18 11:05:28 -0300

    2º commit

--- sigec/lib/Modules/GerenciarHorario/TodosHorarios.dart
+++ sigec/lib/Modules/GerenciarHorario/TodosHorarios.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+
+class Agenda extends StatefulWidget {
+  const Agenda({ Key? key }) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<Agenda> createState() => _AgendaState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _AgendaState extends State<Agenda> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('SIGEC'),
+        backgroundColor:Color.fromARGB(255, 3, 8, 58)
       
+      ),
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      body: Padding(
+      padding: const EdgeInsets.fromLTRB(150, 50, 150, 0),
+      child: SfCalendar(
+          view: CalendarView.month
+        ),
+      )
     );
   }
 }
